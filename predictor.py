9a495e046c3409d181c85f9dbe122b98bc0b0107
torbkle <torkle05553@stud.noroff.no>
2025-09-11 11:46:50 +0200

    Create predictor.py

---
+++ predictor.py
@@ -0,0 +1,15 @@
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+def predict_zero_balance(df_sorted):
+    df_sorted["Dag"] = (df_sorted["Dato"] - df_sorted["Dato"].min()).dt.days
+    X = df_sorted[["Dag"]]
+    y = df_sorted["Saldo"]
+    model = LinearRegression()
+    model.fit(X, y)
+    if model.coef_[0] < 0:
+        dag_null = -model.intercept_ / model.coef_[0]
+        dato_null = df_sorted["Dato"].min() + pd.Timedelta(days=dag_null)
+        return dato_null.date(), model.coef_[0]
+    else:
+        return None, model.coef_[0]
