afe4265db2fd7727365d3cca7122c16aea384497
torbkle <torkle05553@stud.noroff.no>
2025-09-11 12:26:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -111,5 +111,7 @@ elif valg == "➕ Legg til transaksjon":
             submitted = True
 
     if submitted:
-        st.success("Transaksjon lagt til!")
+    st.success("Transaksjon lagt til!")
+    if st.button("🔄 Oppdater visning"):
         st.experimental_rerun()
+
